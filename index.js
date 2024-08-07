279448653ca9067e9950336003e5f49c3c2e025a
YumaArita <98927271+YumaArita@users.noreply.github.com>
2024-08-07 21:09:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,12 @@ const cors = require('cors');
 
 const app = express();
 
-// 手動でCORSヘッダーを設定
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://yumaarita.github.io');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-
-// CORS設定
 const corsOptions = {
   origin: 'https://yumaarita.github.io',
   optionsSuccessStatus: 200,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: ['Content-Type'],
+  allowedHeaders: ['Content-Type', 'X-Requested-With'],
+  credentials: true
 };
 
 app.use(cors(corsOptions));

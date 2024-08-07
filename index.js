1cff2b27ffa876ea780fea64ddcb7abeaf800345
YumaArita <98927271+YumaArita@users.noreply.github.com>
2024-08-07 19:28:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,27 +3,28 @@ const cors = require('cors');
 
 const app = express();
 
+// 手動でCORSヘッダーを設定
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://yumaarita.github.io');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 // CORS設定
 const corsOptions = {
   origin: 'https://yumaarita.github.io',
   optionsSuccessStatus: 200,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ['Content-Type'],
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.set({ 'Access-Control-Allow-Origin': '*' });
-  next();
-});
-
-app.options('/api/verify', cors(corsOptions));
-app.options('/api/generate-token', cors(corsOptions));
-
-app.use('/api/verify', require('./verify'));
-app.use('/api/generate-token', require('./generate-token'));
+app.use('/verificstion-spi/api/verify', require('./verify'));
+app.use('/verificstion-spi/api/generate-token', require('./generate-token'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

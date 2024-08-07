d777caa85a6399b0811d061eadfbce3db208232b
YumaArita <98927271+YumaArita@users.noreply.github.com>
2024-08-07 23:34:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ const corsOptions = {
   optionsSuccessStatus: 200,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ['Content-Type', 'X-Requested-With'],
+  credentials: true
 };
-
-app.get('/favicon.ico', (req, res) => res.status(204));
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -20,13 +19,14 @@ app.use(express.json());
 // プリフライトリクエストを処理
 app.options('*', cors(corsOptions));
 
+app.get('/favicon.ico', (req, res) => res.status(204));
+
 app.use('/api/verify', require('./verification-api/api/verify'));
 app.use('/api/generate-token', require('./verification-api/api/generate-token'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 

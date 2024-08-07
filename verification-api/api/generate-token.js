11d4bbea52d33ae20295b8e7a004570cecaed1ee
YumaArita <98927271+YumaArita@users.noreply.github.com>
2024-08-07 23:35:37 +0900

    Update generate-token.js

--- verification-api/api/generate-token.js
+++ verification-api/api/generate-token.js
@@ -1,24 +1,6 @@
 const tokens = require('./token');
 
 module.exports = (req, res) => {
-  if (req.method === "OPTIONS") {
-    res.set({
-      'Access-Control-Allow-Origin': 'https://yumaarita.github.io',
-      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-      'Access-Control-Allow-Headers': 'X-Requested-With, content-type',
-      'Access-Control-Allow-Credentials': 'true'
-    });
-    res.status(204).end();
-    return;
-  }
-
-  res.set({
-    'Access-Control-Allow-Origin': 'https://yumaarita.github.io',
-    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-    'Access-Control-Allow-Headers': 'X-Requested-With, content-type',
-    'Access-Control-Allow-Credentials': 'true'
-  });
-
   if (req.method === "POST") {
     const { email, token } = req.body;
     tokens[token] = { email, verified: false };
